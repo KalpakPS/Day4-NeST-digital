c854029e49eb6c24c23cc1d3959774c936b93618
Kalpak P S <kalpak9446@gmail.com>
2024-07-28 23:40:09 +0530

    changed country names to button

--- src/app/units/footprint/footprint.component.ts
+++ src/app/units/footprint/footprint.component.ts
@@ -9,4 +9,14 @@ import { RouterLink } from '@angular/router';
   styleUrl: './footprint.component.css'
 })
 export class FootprintComponent {
+  title = 'map-switcher';
+  latitude = 43.0306;  // Default latitude
+  longitude = -88.1134; // Default longitude
+
+  // Method to change the map's location
+  setLocation(lat: number, lng: number) {
+    this.latitude = lat;
+    this.longitude = lng;
+  }
 }
+
